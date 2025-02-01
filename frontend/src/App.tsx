7b97a05fa63a9e492ad0747abec696616eac82ce
Ghafoor Nazari <GhafoorNazari@proton.me>
2025-02-01 23:37:29 +0100

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import FileUpload from "./components/FileUpload";
+import About from "./components/About";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/upload" element={<FileUpload />} />
+        <Route path="/About" element={<About />}></Route>
       </Routes>
     </>
   );
