a9831baf630e6ca5f0ee7696e33b188f5d8dca47
Ghafoor Nazari <GhafoorNazari@proton.me>
2025-02-01 22:29:12 +0100

    Added a simple navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,16 +2,22 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <>
+    <div className="flex justify-between items-center px-4 py-4 bg-gray-800 text-white">
       <div className="navbar-left">
         <h3>Summario</h3>
       </div>
-      <div className="navbar-right">
-        <a href="#">Home</a>
-        <a href="#">Options</a>
-        <a href="#">About</a>
+      <div className="navbar-right space-x-4">
+        <a href="#" className="hover:underline">
+          Home
+        </a>
+        <a href="#" className="hover:underline">
+          Options
+        </a>
+        <a href="#" className="hover:underline">
+          About
+        </a>
       </div>
-    </>
+    </div>
   );
 };
 
