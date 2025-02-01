55c711961fe03e370b76c47014668e74294e4275
Ghafoor Nazari <GhafoorNazari@proton.me>
2025-02-01 22:30:28 +0100

    updated the main file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import Navbar from "./components/Navbar.tsx";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Navbar />
-    <App />
+    <BrowserRouter>
+      <Navbar />
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
